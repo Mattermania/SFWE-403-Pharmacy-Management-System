3cbc6340fdaa0fe50e8670678db84de31dee3501
Matthew Nolan <mdnolan@arizona.edu>
2024-10-14 09:29:59 -0700

    Replaced SQL database with H2 database

--- sfwe403/src/main/java/com/_5guys/domain/Account.java
+++ sfwe403/src/main/java/com/_5guys/domain/Account.java
@@ -9,10 +9,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
-
-import org.hibernate.annotations.UuidGenerator;
 
 @Entity
 @Getter
@@ -21,20 +20,26 @@ import org.hibernate.annotations.UuidGenerator;
 @AllArgsConstructor
 @JsonInclude(NON_DEFAULT)
 @Table(name = "accounts")
-public class Account {
+public class Account {   
     @Id
     @UuidGenerator
-    @Column(name = "id", unique = true, updatable = false)
+    @Column(name = "id", unique = true, updatable = false, nullable = false)
     private String id;
+    @Column(name = "username", unique = true, updatable = true, nullable = false)
     protected String username;
+    @Column(name = "password", unique = false, updatable = true, nullable = false)
     protected String password;
+    @Column(name = "name", unique = false, updatable = true, nullable = false)
     protected String name;
+    @Column(name = "email", unique = true, updatable = true, nullable = false)
     protected String email;
+    @Column(name = "phone_number", unique = false, updatable = true, nullable = false)
     protected String phoneNumber;
+    @Column(name = "address", unique = false, updatable = true, nullable = false)
     protected String address;
     
     private String photoUrl;
-    
+
     // public Account(String username, String password, String name, String email, String phoneNumber, String address) {
     //     this.username = username;
     //     this.password = password;
