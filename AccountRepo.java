2578a57c76bc2bacaec9db65ebb7b8a2509cc2b1
luissoto <143658702+luissotoece@users.noreply.github.com>
2024-11-18 11:35:05 -0700

    Sprint 4

---
+++ AccountRepo.java
@@ -0,0 +1,22 @@
+//Added methods to find an account by username or email. Added methods to update failed login attempts and account lock status.
+package com._5guys.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com._5guys.domain.Account;
+import java.util.Optional;
+
+@Repository
+public interface AccountRepo extends JpaRepository<Account, String> {
+    Optional<Account> findById(String id);
+    Account findByUsernameAndPassword(String username, String password);
+    Account findByEmailAndPassword(String email, String password);
+
+    // **New methods added**
+
+    // Find an account by username
+    Optional<Account> findByUsername(String username);
+
+    // Find an account by email
+    Optional<Account> findByEmail(String email);
+}
