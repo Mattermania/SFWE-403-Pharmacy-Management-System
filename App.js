990da246078ba97c2739e61d32b25cdf3e395dbe
luissoto <143658702+luissotoece@users.noreply.github.com>
2024-11-18 11:50:31 -0700

    Sprint 4

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import Login from './Login';
-import PharmacyStaffGUI from './PharmacyStaffGUI';
+//Add a route for the new UnlockAccounts component.
+
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AddCustomer from "./pages/AddCustomer";
+import ViewCustomers from "./pages/ViewCustomers";
+import TrackPrescriptions from "./pages/TrackPrescriptions"; // Import Track Prescriptions
+import AddPrescription from "./pages/AddPrescription"; // Import Add Prescription
+import Login from "./Login";
+import Forgot from "./pages/Forgot";
+import Signup from "./pages/Signup";
+import Pharmacist from "./pages/Pharmacist";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    const handleLogin = () => {
-        setIsLoggedIn(true); // For now, simulate a successful login
-    };
-
-    return (
-        <div className="App">
-            <Header />
-            {!isLoggedIn ? <Login onLogin={handleLogin} /> : <PharmacyStaffGUI />}
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/forgotpassword" element={<Forgot />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/beforepharm" element={<HomePage />} />
+        <Route path="/pharmacist" element={<Pharmacist />} />
+        <Route path="/add-customer" element={<AddCustomer />} />
+        <Route path="/view-customers" element={<ViewCustomers />} />
+        <Route path="/track-prescriptions" element={<TrackPrescriptions />} />
+        <Route path="/add-prescription" element={<AddPrescription />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+
+//New implementation
+import UnlockAccounts from "./pages/UnlockAccounts"; // Import the new component
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Existing routes */}
+        <Route path="/unlock-accounts" element={<UnlockAccounts />} /> {/* New route */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
