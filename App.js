3401fe073aedb338e60f7873f8be6012556e95b2
luissoto <143658702+luissotoece@users.noreply.github.com>
2024-11-18 12:30:40 -0700

    Sprint 4

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-//Add a route for the new UnlockAccounts component.
-
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AddCustomer from "./pages/AddCustomer";
 import ViewCustomers from "./pages/ViewCustomers";
-import TrackPrescriptions from "./pages/TrackPrescriptions"; // Import Track Prescriptions
-import AddPrescription from "./pages/AddPrescription"; // Import Add Prescription
+import TrackPrescriptions from "./pages/TrackPrescriptions";
+import AddPrescription from "./pages/AddPrescription";
 import Login from "./Login";
 import Forgot from "./pages/Forgot";
 import Signup from "./pages/Signup";
 import Pharmacist from "./pages/Pharmacist";
+import UnlockAccounts from "./pages/UnlockAccounts"; // Import the new component
 
 function App() {
   return (
@@ -23,27 +22,22 @@ function App() {
         <Route path="/pharmacist" element={<Pharmacist />} />
         <Route path="/add-customer" element={<AddCustomer />} />
         <Route path="/view-customers" element={<ViewCustomers />} />
-        <Route path="/track-prescriptions" element={<TrackPrescriptions />} />
-        <Route path="/add-prescription" element={<AddPrescription />} />
+        <Route
+          path="/track-prescriptions"
+          element={<TrackPrescriptions />}
+        />
+        <Route
+          path="/add-prescription"
+          element={<AddPrescription />}
+        />
+        <Route
+          path="/unlock-accounts"
+          element={<UnlockAccounts />}
+        />{" "}
+        {/* New route */}
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
-
-//New implementation
-import UnlockAccounts from "./pages/UnlockAccounts"; // Import the new component
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Existing routes */}
-        <Route path="/unlock-accounts" element={<UnlockAccounts />} /> {/* New route */}
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
